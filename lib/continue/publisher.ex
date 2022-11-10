f01e00788209c9ede4ac6b2a8ae8bccea41ef829
Andreas Tell <git@tell.nu>
2022-11-10 22:32:17 +0100

    add Continue.Publisher
    Implements Collectable; uses Phoenix.PubSub.

---
+++ lib/continue/publisher.ex
@@ -0,0 +1,21 @@
+defmodule Continue.Publisher do
+  alias Phoenix.PubSub
+
+  defstruct topic: nil
+
+  def new(topic) do
+    %Continue.Publisher{topic: topic}
+  end
+
+  defimpl Collectable, for: Continue.Publisher do
+    def into(%{topic: topic} = publisher) do
+      {:ok, fn
+        :ok, {:cont, message} ->
+          PubSub.broadcast(Continue.PubSub, topic, {:out, message})
+        
+        :ok, _ ->
+          publisher
+      end}
+    end
+  end
+end
