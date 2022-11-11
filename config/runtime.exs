07645a2258a83a4debff871766d74c01b40aceb2
Andreas Tell <git@tell.nu>
2022-11-11 17:01:16 +0100

    add production config

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,46 +1,14 @@
 import Config
 
-# config/runtime.exs is executed for all environments, including
-# during releases. It is executed after compilation and before the
-# system starts, so it is typically used to load production configuration
-# and secrets from environment variables or elsewhere. Do not define
-# any compile-time configuration in here, as it won't be applied.
-# The block below contains prod specific runtime configuration.
+# Start the phoenix server if environment is set and running in a release
 
-# ## Using releases
-#
-# If you use `mix release`, you need to explicitly enable the server
-# by passing the PHX_SERVER=true when you start it:
-#
-#     PHX_SERVER=true bin/continue start
-#
-# Alternatively, you can use `mix phx.gen.release` to generate a `bin/server`
-# script that automatically sets the env var above.
-if System.get_env("PHX_SERVER") do
+if System.get_env("PHX_SERVER") && System.get_env("RELEASE_NAME") do
   config :continue, ContinueWeb.Endpoint, server: true
 end
 
 if config_env() == :prod do
-  database_url =
-    System.get_env("DATABASE_URL") ||
-      raise """
-      environment variable DATABASE_URL is missing.
-      For example: ecto://USER:PASS@HOST/DATABASE
-      """
+  # Env
 
-  maybe_ipv6 = if System.get_env("ECTO_IPV6"), do: [:inet6], else: []
-
-  config :continue, Continue.Repo,
-    # ssl: true,
-    url: database_url,
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-    socket_options: maybe_ipv6
-
-  # The secret key base is used to sign/encrypt cookies and other secrets.
-  # A default value is used in config/dev.exs and config/test.exs but you
-  # want to use a different value for prod and you most likely don't want
-  # to check this value into version control, so we use an environment
-  # variable instead.
   secret_key_base =
     System.get_env("SECRET_KEY_BASE") ||
       raise """
@@ -48,18 +16,81 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
-  port = String.to_integer(System.get_env("PORT") || "4000")
+  database_url =
+    System.get_env("DATABASE_URL") ||
+      raise """
+      environment variable DATABASE_URL is missing.
+      For example: ecto://USER:PASS@HOST/DATABASE
+      """
+
+  logflare_api_key =
+    System.get_env("LOGFLARE_API_KEY") ||
+      raise """
+      environment variable LOGFLARE_API_KEY is missing.
+      """
+
+  logflare_source_id =
+    System.get_env("LOGFLARE_SOURCE_ID") ||
+      raise """
+      environment variable LOGFLARE_SOURCE_ID is missing.
+      """
+
+  ssl_key_path =
+    System.get_env("SSL_KEY_PATH") ||
+      raise """
+      environment variable SSL_KEY_PATH is missing.
+      """
+
+  ssl_cert_path =
+    System.get_env("SSL_CERT_PATH") ||
+      raise """
+      environment variable SSL_CERT_PATH is missing.
+      """
+
+  http_port =
+    System.get_env("HTTP_PORT") ||
+      raise """
+      environment variable HTTP_PORT is missing.
+      """
+
+  https_port =
+    System.get_env("HTTPS_PORT") ||
+      raise """
+      environment variable HTTPS_PORT is missing.
+      """
+
+  # Ecto
+
+  maybe_ipv6 = if System.get_env("ECTO_IPV6"), do: [:inet6], else: []
+
+  config :continue, Continue.Repo,
+    ssl: true,
+    url: database_url,
+    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+    socket_options: maybe_ipv6,
+    start_apps_before_migration: [:ssl]
+
+  # LogFlare
+
+  config :logflare_logger_backend,
+    url: "https://api.logflare.app",
+    level: :info,
+    api_key: logflare_api_key,
+    source_id: logflare_source_id,
+    flush_interval: 1_000,
+    max_batch_size: 50,
+    metadata: :all
+
+  # Host / SSL
 
   config :continue, ContinueWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
-    http: [
-      # Enable IPv6 and bind on all interfaces.
-      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-      # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
-      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: port
+    url: [scheme: "https", host: "continue.tell.nu", port: 443],
+    http: [port: http_port],
+    https: [
+      port: https_port,
+      cipher_suite: :strong,
+      keyfile: ssl_key_path,
+      certfile: ssl_cert_path
     ],
     secret_key_base: secret_key_base
 
